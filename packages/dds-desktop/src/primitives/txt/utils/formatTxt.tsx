9efc1bf33e5086e77fd6c3135afc9f819d36dd1b
Zero-1016 <iolpd42@gmail.com>
2026-04-09 17:11:30 +0900

    fix(desktop): Txt 이스케이프/리터럴 개행 문자 모두 줄바꿈 처리

--- packages/dds-desktop/src/primitives/txt/utils/formatTxt.tsx
+++ packages/dds-desktop/src/primitives/txt/utils/formatTxt.tsx
@@ -2,8 +2,10 @@ import { Fragment } from 'react'
 
 export const withLineBreaks = (txt: string | undefined) => {
   if (!txt) return null
-  return txt.split('/n').map((line, i) => {
-    if (line === '/n') return <br key={i} />
-    return <Fragment key={line}>{line}</Fragment>
-  })
+  return txt.split(/\\n|\n/).map((line, i, arr) => (
+    <Fragment key={i}>
+      {line}
+      {i < arr.length - 1 && <br />}
+    </Fragment>
+  ))
 }
