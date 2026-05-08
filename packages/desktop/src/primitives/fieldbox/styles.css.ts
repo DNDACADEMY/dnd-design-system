1af7fe0cd70fb5ebff8ac366c2c4b72eb204e6ee
Zero-1016 <iolpd42@gmail.com>
2026-05-08 16:47:29 +0900

    feat(desktop): fieldbox 디자인 변경 반영

--- packages/desktop/src/primitives/fieldbox/styles.css.ts
+++ packages/desktop/src/primitives/fieldbox/styles.css.ts
@@ -2,17 +2,33 @@ import { color } from '@dnd-lab/token'
 import { createVar, style } from '@vanilla-extract/css'
 import { recipe } from '@vanilla-extract/recipes'
 
+import { FieldBoxSize } from './type'
+
 export const fieldboxContainerCss = style({
   display: 'flex',
   flexDirection: 'column',
   gap: '4px'
 })
 
-const fieldBoxContentVariants = {
+export const fieldBoxContentVariants = {
   minHeight: createVar(),
   backgroundColor: createVar(),
   borderColor: createVar(),
   gap: createVar()
+} as const
+
+const FIELDBOX_VARIANTS = {
+  MIN_HEIGHT: {
+    small: '32px',
+    medium: '42px',
+    large: '48px'
+  } satisfies Record<FieldBoxSize, string>,
+  ADDON_GAP: {
+    small: '4px',
+    medium: '8px',
+    large: '8px'
+  } satisfies Record<FieldBoxSize, string>,
+  RADIUS: 6
 } as const
 
 export const fieldboxContentCss = recipe({
@@ -23,18 +39,23 @@ export const fieldboxContentCss = recipe({
     alignItems: 'center',
     gap: fieldBoxContentVariants.gap,
     minHeight: fieldBoxContentVariants.minHeight,
-    borderRadius: 6,
+    borderRadius: FIELDBOX_VARIANTS.RADIUS,
     backgroundColor: fieldBoxContentVariants.backgroundColor,
     border: `1px solid ${fieldBoxContentVariants.borderColor}`,
     transition: 'border-color 0.2s ease-in-out',
     vars: {
-      [fieldBoxContentVariants.backgroundColor]: color.primitive.mono.white,
-      [fieldBoxContentVariants.borderColor]: color.semantic.border.neutral.strong
+      [fieldBoxContentVariants.backgroundColor]: color.semantic.background.neutral.canvas,
+      [fieldBoxContentVariants.borderColor]: color.semantic.border.neutral.medium
     },
     selectors: {
+      '&:hover': {
+        vars: {
+          [fieldBoxContentVariants.borderColor]: color.semantic.border.neutral.hover
+        }
+      },
       '&:focus-within': {
         vars: {
-          [fieldBoxContentVariants.borderColor]: color.primitive.cyan['400']
+          [fieldBoxContentVariants.borderColor]: color.semantic.border.neutral.focus
         }
       }
     }
@@ -44,29 +65,22 @@ export const fieldboxContentCss = recipe({
       small: {
         padding: '0 16px',
         vars: {
-          [fieldBoxContentVariants.minHeight]: '32px',
-          [fieldBoxContentVariants.gap]: '4px'
+          [fieldBoxContentVariants.minHeight]: FIELDBOX_VARIANTS.MIN_HEIGHT.small,
+          [fieldBoxContentVariants.gap]: FIELDBOX_VARIANTS.ADDON_GAP.small
         }
       },
       medium: {
         padding: '0 16px',
         vars: {
-          [fieldBoxContentVariants.minHeight]: '42px',
-          [fieldBoxContentVariants.gap]: '8px'
+          [fieldBoxContentVariants.minHeight]: FIELDBOX_VARIANTS.MIN_HEIGHT.medium,
+          [fieldBoxContentVariants.gap]: FIELDBOX_VARIANTS.ADDON_GAP.medium
         }
       },
       large: {
         padding: '0 16px',
         vars: {
-          [fieldBoxContentVariants.minHeight]: '48px',
-          [fieldBoxContentVariants.gap]: '8px'
-        }
-      }
-    },
-    error: {
-      true: {
-        vars: {
-          [fieldBoxContentVariants.borderColor]: color.semantic.border.error.medium
+          [fieldBoxContentVariants.minHeight]: FIELDBOX_VARIANTS.MIN_HEIGHT.large,
+          [fieldBoxContentVariants.gap]: FIELDBOX_VARIANTS.ADDON_GAP.large
         }
       }
     },
@@ -74,24 +88,51 @@ export const fieldboxContentCss = recipe({
       true: {
         pointerEvents: 'none',
         vars: {
-          [fieldBoxContentVariants.backgroundColor]: color.primitive.slate['100'],
-          [fieldBoxContentVariants.borderColor]: color.primitive.slate['300']
+          [fieldBoxContentVariants.backgroundColor]: color.semantic.background.neutral.disabled,
+          [fieldBoxContentVariants.borderColor]: color.semantic.border.neutral.disabled
         }
       }
     },
     readonly: {
       true: {
         vars: {
-          [fieldBoxContentVariants.backgroundColor]: color.semantic.background.neutral.primary,
-          [fieldBoxContentVariants.borderColor]: color.primitive.slate['100']
+          [fieldBoxContentVariants.backgroundColor]: color.semantic.background.neutral.tertiary,
+          [fieldBoxContentVariants.borderColor]: color.semantic.border.neutral.medium
+        }
+      }
+    },
+    error: {
+      true: {
+        vars: {
+          [fieldBoxContentVariants.backgroundColor]: color.semantic.background.neutral.canvas,
+          [fieldBoxContentVariants.borderColor]: color.semantic.border.error.strong
         }
       }
     }
   }
 })
 
-export const bottomTxtCss = style({
-  margin: 0
+const bottomTxtVariants = {
+  color: createVar()
+} as const
+
+export const bottomTxtCss = recipe({
+  base: {
+    margin: 0,
+    color: bottomTxtVariants.color,
+    vars: {
+      [bottomTxtVariants.color]: color.semantic.text.neutral.quaternary
+    }
+  },
+  variants: {
+    error: {
+      true: {
+        vars: {
+          [bottomTxtVariants.color]: color.semantic.text.error.primary
+        }
+      }
+    }
+  }
 })
 
 export const requiredStyleCss = style({
